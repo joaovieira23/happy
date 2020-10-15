66267bb5a767ffb74a3684dd12bc221e5cb737d6
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-10-14 23:41:33 -0300

    chore: configure multer

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from './config/upload';
 import InstitutionsController from './controllers/institutionsController';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get('/institution', InstitutionsController.index);
 routes.get('/institution/:id', InstitutionsController.show);
-routes.post('/institution', InstitutionsController.create);
+routes.post('/institution', upload.array('images'), InstitutionsController.create);
 
 export default routes;
