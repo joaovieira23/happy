36f8f83b822f408380c3fbf597d954d748ca3524
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-10-15 14:20:07 -0300

    feat: validate data

--- backend/src/controllers/institutionsController.ts
+++ backend/src/controllers/institutionsController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import institutionView from '../views/institution_view';
+import * as Yup from 'yup';
 
 import Institution from '../models/Institution';
 
@@ -47,7 +48,7 @@ export default {
       return { path: image.filename }
     })
 
-    const institution = institutionRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -56,7 +57,26 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(Yup.object().shape({
+        path: Yup.string().required()
+      }))
     });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    })
+
+    const institution = institutionRepository.create(data);
 
     await institutionRepository.save(institution);
 
